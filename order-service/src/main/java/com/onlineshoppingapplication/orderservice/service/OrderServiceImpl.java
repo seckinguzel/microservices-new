46e8229ac58433fc7fdd85ba9d86345e4efec10f
seckinguzel <seckinguzel@outlook.com>
2023-06-12 16:29:15 +0300

    Event Driven Architecture using Kafka has been added to the project, in this scope new microservice is added.

--- order-service/src/main/java/com/onlineshoppingapplication/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/onlineshoppingapplication/orderservice/service/OrderServiceImpl.java
@@ -3,10 +3,15 @@ package com.onlineshoppingapplication.orderservice.service;
 import com.onlineshoppingapplication.orderservice.dto.InventoryResponse;
 import com.onlineshoppingapplication.orderservice.dto.OrderLineItemsDto;
 import com.onlineshoppingapplication.orderservice.dto.OrderRequest;
+import com.onlineshoppingapplication.orderservice.event.OrderPlacedEvent;
 import com.onlineshoppingapplication.orderservice.model.Order;
 import com.onlineshoppingapplication.orderservice.model.OrderLineItems;
 import com.onlineshoppingapplication.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,9 +23,12 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     @Override
     public String placeOrder(OrderRequest orderRequest) {
@@ -38,22 +46,32 @@ public class OrderServiceImpl implements OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        //Below operations are created for to call inventory service, place order if product is in stock.
-        //bodyToMono is for to read data coming from webClient response. We get answer boolean so why we wrote boolean to the bodyToMono.
-        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block(); //webClient will make a synchronous request to the uri endpoint.
+        log.info("Calling inventory service.");
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock); //inventoryResponseArray.st yazaıp otomatik stream le tamamladığımda ifadeyi Arrays.stream haline getiriyor. Ardından allMatch(inventoryResponse -> yapıp inventoryResponse.isInStock()) dedikten sonra sarı uyarıya tıklayıp introduce local variable diyip bu hale getirdim.
+        //Zipkin trace yapısını kullanmak için aşağıdaki Span ve Tracer yapısını kullandık.
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
 
-        if (allProductsInStock) {
-            orderRepository.save(order); //eğer tüm ürünler stokta ise database'e kaydet.
-            return "Order placed successfully!";
-        } else {
-            throw new IllegalArgumentException("Product is not in stock! Please try again later.");
+        try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+            //Below operations are created for to call inventory service, place order if product is in stock.
+            //bodyToMono is for to read data coming from webClient response. We get answer boolean so why we wrote boolean to the bodyToMono.
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block(); //webClient will make a synchronous request to the uri endpoint.
+
+            boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock); //inventoryResponseArray.st yazaıp otomatik stream le tamamladığımda ifadeyi Arrays.stream haline getiriyor. Ardından allMatch(inventoryResponse -> yapıp inventoryResponse.isInStock()) dedikten sonra sarı uyarıya tıklayıp introduce local variable diyip bu hale getirdim.
+
+            if (allProductsInStock) {
+                orderRepository.save(order); //eğer tüm ürünler stokta ise database'e kaydet.
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order placed successfully!";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock! Please try again later.");
+            }
+        } finally {
+            inventoryServiceLookup.end();
         }
     }
 
